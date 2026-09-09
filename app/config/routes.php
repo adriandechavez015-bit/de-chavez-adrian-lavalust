779612d03d48eaf1b212c44ff9d18fd966c04ecb
Adrian De Chavez <adriandechavez015@email.com>
2026-09-09 18:48:34 +0800

    Move welcome route to /welcome to allow AuthController default route

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,8 +44,9 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-// Preserved Welcome Page under /welcome
-$router->get('/welcome', 'Welcome::index');
+$router->get('/', 'Welcome::index');
+
+
 
 $router->get('/student', 'StudentController::index');
 $router->get('/student/profile', 'StudentController::profile', ['middleware' => ['student']]);
@@ -54,16 +55,21 @@ $router->get('/student/clear', 'StudentController::clear');
 $router->get('/users', 'UsersController::index');
 
 
+
+
+
 // Default home route points to the login page
-$route['default_controller'] = 'AuthController/login';
-$route['login']             = 'AuthController/login';
+$route['default_controller'] = 'AuthController';
+$route['login']             = 'AuthController';
 $route['login/submit']      = 'AuthController/login_submit';
 $route['logout']            = 'AuthController/logout';
 
 // Protected Product CRUD routes
-$route['products']               = 'ProductController/index';
-$route['products/create']        = 'ProductController/create';
-$route['products/store']         = 'ProductController/store';
-$route['products/edit/(:num)']   = 'ProductController/edit/$1';
-$route['products/update/(:num)'] = 'ProductController/update/$1';
-$route['products/delete/(:num)'] = 'ProductController/delete/$1';
+$route['products']                = 'ProductController/index';
+$route['products/create']         = 'ProductController/create';
+$route['products/store']          = 'ProductController/store';
+$route['products/edit/(:num)']    = 'ProductController/edit/$1';
+$route['products/update/(:num)']  = 'ProductController/update/$1';
+$route['products/delete/(:num)']  = 'ProductController/delete/$1';
+
+
